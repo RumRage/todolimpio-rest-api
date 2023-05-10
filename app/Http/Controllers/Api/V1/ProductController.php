34050058d1a8bbcf48a4f0225958c79b81ef260e
Rumrage <claravillarruel4@gmail.com>
2023-05-10 20:42:50 -0300

    Recuperado Products

---
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreProductRequest;
+use App\Models\Product;
+use App\Http\Resources\V1\ProductResource;
+use App\Http\Resources\V1\ProductCollection;
+
+class ProductController extends Controller
+{
+    //
+ 
+    public function index()
+    {
+        return new ProductCollection(Product::all());
+    }
+    
+    public function store(StoreProductRequest $request)
+    {
+        Product::create($request->validated());
+        return response()->json("Producto creado correctamente");
+    }
+    
+    public function update(StoreProductRequest $request, Product $product)
+    {
+        $product->update($request->validated());
+        return response()->json("Producto actualizado correctamente");
+    }
+
+    public function show(Product $product)
+    {
+        return new ProductResource($product);
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response()->json("Producto eliminado correctamente");
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\CategoryController;
+use App\Http\Controllers\Api\V1\ProductController;
 
 
 Route::group(['prefix' => 'v1'], function() {
     Route::apiResource('categories', CategoryController::class);
     });
+
+Route::group(['prefix' => 'v1'], function() {
+    Route::apiResource('products', ProductController::class);
+    });
+
