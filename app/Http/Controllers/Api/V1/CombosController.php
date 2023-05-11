6b673ffb6f1e5c681e92badfdac1bda5adc6c20f
Rumrage <claravillarruel4@gmail.com>
2023-05-10 22:39:52 -0300

    Update Combo

--- app/Http/Controllers/Api/V1/CombosController.php
+++ app/Http/Controllers/Api/V1/CombosController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreComboRequest;
 use App\Models\Combo;
+use App\Http\Resources\V1\ComboResource;
 
 
 class CombosController extends Controller
@@ -19,16 +20,34 @@ class CombosController extends Controller
 
     public function store(StoreComboRequest $request)
     {
-        Combo::create($request->validated());
+        $combo = Combo::create($request->validated());
+        
+        // si se proporcionaron servicios en el request
+        if ($request->has('service_id')) {
+            // obtener los ids de los servicios
+            $serviceIds = $request->input('service_id');
+            // agregar la relación many-to-many
+            $combo->services()->attach($serviceIds);
+        }
+        
         return response()->json("Combo creado correctamente");
     }
+    
 
     public function update(StoreComboRequest $request, Combo $combo)
     {
         $combo->update($request->validated());
+    
+        // Sincronizar servicios del combo actualizado
+        $combo->services()->sync($request->input('service_id', []));
+    
         return response()->json("Combo actualizado correctamente");
     }
 
-    
+
+    public function show(Combo $combo)
+    {
+        return new ComboResource($combo);
+    }
 
 }
---
+++ app/Http/Resources/V1/ComboResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ComboResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+    return [
+        'id' => $this->id,
+        'name' => $this->name,
+        'price' => $this->price,
+        'discount' => $this->discount,
+        'total_price' => $this->total_price
+        ];
+    }
+}
