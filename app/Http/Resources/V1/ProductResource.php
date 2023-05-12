4e32b126a02e24a29bb94f48dfc27cbe6a937ec1
Rumrage <claravillarruel4@gmail.com>
2023-05-12 13:34:01 -0300

    Fix error git

--- app/Http/Resources/V1/ProductResource.php
+++ app/Http/Resources/V1/ProductResource.php
@@ -14,21 +14,13 @@ class ProductResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-<<<<<<< HEAD
-        return [
-=======
     return [
->>>>>>> a0a0412fc406268adaf849b81856f8f8f63919a8
         'id' => $this->id,
         'name' => $this->name,
         'description' => $this->description,
         'price' => $this->price,
         'supplier' => $this->supplier,
         'stock' => $this->stock
-<<<<<<< HEAD
-        ];
-=======
-    ];
->>>>>>> a0a0412fc406268adaf849b81856f8f8f63919a8
+    ];    
     }
 }
